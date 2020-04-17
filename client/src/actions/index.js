62470c623ac2b2c90db1dac8465832b4f0f8c8ac
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-04-17 23:14:37 +0300

    Introduced JSON Server

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,20 @@
-import { SIGN_IN, SIGN_OUT } from './types'
+import { SIGN_IN, SIGN_OUT } from "./types";
+import streams from "../apis/streams";
 
 export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    }
-}
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    }
-}
+  return {
+    type: SIGN_OUT,
+  };
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    streams.post("/streams", formValues);
+  };
+};
