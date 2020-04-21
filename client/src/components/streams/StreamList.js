c584a38a38c7291884f5f8dcc96be20b545a95a2
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-04-21 08:48:44 +0300

    Stream creation
    - streams are now created with a user id

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    let streamElements = this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle algined icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+    return streamElements;
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams), // turn the values into an array
+  };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
